343445f847df6f906f05b8eae09ceb6c096a5133
publichealth-edu <87641932+publichealth-edu@users.noreply.github.com>
2021-10-22 23:22:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -71,12 +71,12 @@ def login():
         filename = files.save(reference)
 
         with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
-        connection.starttls()
-        connection.login(user=FROM_EMAIL, password=PASSWORD)
-        connection.sendmail(
-            from_addr=FROM_EMAIL,
-            to_addrs=TO_EMAIL,
-            msg=f"Subject: New APH Membership Application\n\nFirst name: {fname}\n\nMiddle name: {mname}\n\nLast name: {lname}\n\nEmail: {email}\n\nPhone: {phone}\n\nCountry of Citizenship: {citizen}\n\nCountry of Residence: {residence}\n\nCategory being applied for: {category}"
+            connection.starttls()
+            connection.login(user=FROM_EMAIL, password=PASSWORD)
+            connection.sendmail(
+                from_addr=FROM_EMAIL,
+                to_addrs=TO_EMAIL,
+                msg=f"Subject: New APH Membership Application\n\nFirst name: {fname}\n\nMiddle name: {mname}\n\nLast name: {lname}\n\nEmail: {email}\n\nPhone: {phone}\n\nCountry of Citizenship: {citizen}\n\nCountry of Residence: {residence}\n\nCategory being applied for: {category}"
                     )
         
         
